173f3d55ebbdd490d1c6e00c095ceec415b074ba
Anton Rivera <anton.rivera@gmail.com>
2014-06-08 15:32:50 -0700

    initial commit

---
+++ Diary/ARCoreDataStack.h
@@ -0,0 +1,13 @@
+//
+//  ARCoreDataStack.h
+//  Diary
+//
+//  Created by Anton Rivera on 6/8/14.
+//  Copyright (c) 2014 Anton Hilario Rivera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ARCoreDataStack : NSObject
+
+@end
---
+++ Diary/ARCoreDataStack.m
@@ -0,0 +1,13 @@
+//
+//  ARCoreDataStack.m
+//  Diary
+//
+//  Created by Anton Rivera on 6/8/14.
+//  Copyright (c) 2014 Anton Hilario Rivera. All rights reserved.
+//
+
+#import "ARCoreDataStack.h"
+
+@implementation ARCoreDataStack
+
+@end
