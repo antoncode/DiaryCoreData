37de919064b554bf922f4e34a35e318d32ec8594
Anton Rivera <anton.rivera@gmail.com>
2014-06-10 21:17:41 -0700

    set up fetch results

---
+++ Diary/AREntryListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AREntryListTableViewController.h
+//  Diary
+//
+//  Created by Anton Rivera on 6/10/14.
+//  Copyright (c) 2014 Anton Hilario Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AREntryListTableViewController : UITableViewController
+
+@end
