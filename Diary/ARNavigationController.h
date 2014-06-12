bce38b6d568de8cd38b13e8ecdb823b21f6eba9e
Anton Rivera <anton.rivera@gmail.com>
2014-06-11 22:06:37 -0700

    customize navigation bar appearance

---
+++ Diary/ARNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  ARNavigationController.h
+//  Diary
+//
+//  Created by Anton Rivera on 6/11/14.
+//  Copyright (c) 2014 Anton Hilario Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARNavigationController : UINavigationController
+
+@end
---
+++ Diary/ARNavigationController.m
@@ -0,0 +1,22 @@
+//
+//  ARNavigationController.m
+//  Diary
+//
+//  Created by Anton Rivera on 6/11/14.
+//  Copyright (c) 2014 Anton Hilario Rivera. All rights reserved.
+//
+
+#import "ARNavigationController.h"
+
+@interface ARNavigationController ()
+
+@end
+
+@implementation ARNavigationController
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;    // Makes everything in the status bar white
+}
+
+@end
